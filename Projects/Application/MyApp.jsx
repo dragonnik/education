509601411066a45be3fefe7f5da1aa89b45cab36
nikita_tarassov <n.tarassov2003@gmail.com>
2021-06-21 15:52:49 +0600

    m.route

--- Projects/Application/MyApp.jsx
+++ Projects/Application/MyApp.jsx
@@ -3,15 +3,16 @@ import m from 'mithril'
 import { NavBar } from './header/Header.jsx'
 import { Project1 } from './pages/page_1/project_01.jsx'
 export { Menu }
+export { Page1 }
 
 var Home = {
     view: function() {
         return [
             m(Menu),
-            m(NavBar)
         ]
     }
-}
+}   
+
 var Page1 = {
     view: function() {
         return [
@@ -20,12 +21,12 @@ var Page1 = {
         ]
     }
 }
+
 var Menu = {
     view: function(attrs) {
-        attrs.name = this.page1
         return m(NavBar, [
             m(m.route.Link, {href: "/"}),
-            m(m.route.Link, {href: '/page1'}, this.page1)
+            m(m.route.Link, {href: '/page1'})
         ])
     }
 }
--- Projects/Application/header/Header.jsx
+++ Projects/Application/header/Header.jsx
@@ -2,11 +2,13 @@ import m from 'mithril'
 
 import './_header.scss'
 import { Project1 } from '../pages/page_1/project_01.jsx'
+import { Page1 } from '../MyApp.jsx'
 
 export class NavBar{
     constructor(attrs){
         this.menuOpen = false
         this.menuClosed = false
+        
     }
     view(attrs) {
         let classmenuanimatable = ' menu--animatable';
@@ -19,8 +21,11 @@ export class NavBar{
             <div>
                 <div class='layout'>
                     <div class='navbar'>
-                        <div class='dev1'><a href='/'>HOME</a></div>
-                        <div class='dev1'><a href='/page1'>PAGE 1</a></div>
+                        <nav>
+                            <a class='dev1' href='/'>HOME</a>
+                            <br/>
+                            <a class='dev1' href={ Page1 }>PAGE 1</a>
+                        </nav>
                     </div>
                     <div class='frame1'>
                     </div>
